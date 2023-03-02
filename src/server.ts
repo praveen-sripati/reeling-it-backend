fb0956d493d5bd3c5fc3a085dc54b4ba1f6e454f
Praveen Sripati <pvnsripati@gmail.com>
2023-03-03 02:25:33 +0530

    feat: Add api rate limiter to avoid security issues

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { CustomError, customErrorMiddleware, errorType } from './error';
 import { createNewUser, signIn } from './handlers/user';
 import { protect } from './modules/auth';
 import router from './router';
+import rateLimit from 'express-rate-limit';
 import {
   validateInputs,
   validateSignInInputs,
@@ -13,10 +14,18 @@ import {
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.get('/', (req, res) => {
   // console.log('hello from express');
